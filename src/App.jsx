cb6725192e00765eec001b195b57f39fac15afbf
Bryton Smith <bryton.jay.smith@gmail.com>
2020-12-16 14:38:24 -0700

    init
    init one

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+//import Earth
+import Earth from './Components/Earth.jsx';
+
+const App = () => {
+return(
+  <Earth />
+    )
+  };
+
+export default App;
